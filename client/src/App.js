5bf11e26322f9c4a63652e1bbb782876a9b3dc1b
Johnny Li <reptile18@gmail.com>
2020-08-25 13:12:52 -0700

    Google login (#15)
    * add material-ui
    
    * temporarily axe the answers node
    
    * include MERN scripts and dependency
    
    * also install before building
    
    * add package-lock.json
    
    * working Google Login

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route  } from "react-router-dom";
 import PrivacyPolicy from './components/Login/PrivacyPolicy';
 import TOS from './components/Login/TOS';
-import GoogleLogin from './components/Login/GoogleLogin';
+import GoogleButton from './components/Login/GoogleButton';
 
 function App() {
   return (
     <Router>
       <div className="App">
       <Switch>
-        <Route path="/login" exact component={GoogleLogin} />
+        <Route path="/login" exact component={GoogleButton} />
         <Route path="/privacy" exact component={PrivacyPolicy} />
         <Route path="/tos" component={TOS} />
       </Switch>
