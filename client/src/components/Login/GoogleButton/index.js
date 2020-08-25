a413a71ec1014b01295091e81ecdb0b052751c65
Johnny Li <reptile18@gmail.com>
2020-08-25 12:53:30 -0700

    working Google Login

---
+++ client/src/components/Login/GoogleButton/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+const CLIENT_ID = '15003138507-rrf8me4d9tdffufbu8l38mji2h372j2m.apps.googleusercontent.com';
+
+export default class GoogleBtn extends React.Component {
+   constructor(props) {
+    super(props);
+
+    this.state = {
+      isLogined: false,
+      accessToken: '',
+      googleId: ''
+    };
+
+    this.login = this.login.bind(this);
+    this.handleLoginFailure = this.handleLoginFailure.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
+  }
+
+  login (response) {
+    if(response.accessToken){
+      this.setState(state => ({
+        isLogined: true,
+        accessToken: response.accessToken,
+        googleId: response.googleId
+      }));
+      console.log(response.profileObj.email);
+    }
+  }
+
+  logout (response) {
+    this.setState(state => ({
+      isLogined: false,
+      accessToken: ''
+    }));
+  }
+
+  handleLoginFailure (response) {
+    alert('Failed to log in')
+  }
+
+  handleLogoutFailure (response) {
+    alert('Failed to log out')
+  }
+
+  render() {
+    return (
+    <div>
+      { this.state.isLogined ?
+        <GoogleLogout
+          clientId={ CLIENT_ID }
+          buttonText='Logout'
+          onLogoutSuccess={ this.logout }
+          onFailure={ this.handleLogoutFailure }
+        >
+        </GoogleLogout>: <GoogleLogin
+          clientId={ CLIENT_ID }
+          buttonText='Login'
+          onSuccess={ this.login }
+          onFailure={ this.handleLoginFailure }
+          cookiePolicy={ 'single_host_origin' }
+          responseType='code,token'
+        />
+      }
+      { this.state.accessToken ? <h5>Your Access Token: <br/><br/> { this.state.accessToken }</h5> : null }
+      { this.state.googleId ? <h5>Your GoogleId: <br/><br/> { this.state.googleId }</h5> : null }
+    </div>
+    )
+  }
+}
