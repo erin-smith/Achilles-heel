aa17d19648840449390b6795930d6c5f15ede3c9
Johnny Li <reptile18@gmail.com>
2020-08-25 16:08:40 -0700

    basic routes

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+
+function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">
+          Achilles Heel
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
