bfc5e276f755335d5d91b4774a291c315b6357a5
Johnny Li <reptile18@gmail.com>
2020-08-26 14:12:05 -0700

    add utils API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,31 @@
 import axios from "axios";
 
 export default {
-  
+  createUser: function(userData) {
+    return axios.post("/api/user", userData);
+  },
+  saveUser: function(displayName, userData) {
+    return axios.put("/api/user?display_name=" + displayName, userData);
+  },
+  getUser: function(displayName) {
+    return axios.get("/api/user?display_name=" + displayName);
+  },
+  createOverworld: function(worldData) {
+    return axios.post("/api/world", worldData);
+  },
+  findOverworld: function(worldName) {
+    return axios.get("/api/world?name=" + worldName);
+  },
+  createArena: function(arenaData) {
+    return axios.post("/api/level", arenaData);
+  },
+  findArena: function(arenaID) {
+    return axios.get("/api/level?id=" + arenaID);
+  },
+  createQuestion: function(questionData) {
+    return axios.post("/api/questions", questionData);
+  },
+  findQuestions: function(questionTopic) {
+    return axios.get("/api/questions?topic=" + questionTopic);
+  }
 };
