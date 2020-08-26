d35107c68361294d3dcbae06e6d0722885aea419
Johnny Li <reptile18@gmail.com>
2020-08-26 11:48:06 -0700

    more api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
 //const gameRoutes = require("./game");
 const userRoutes = require("./user");
+const questionsRoutes = require("./questions");
+const worldRoutes = require("./world");
+const levelRoutes = require("./level");
 
 // API routes
 //router.use("/game", gameRoutes);
 router.use("/user", userRoutes);
+router.use("/questions", questionsRoutes);
+router.use("/world", worldRoutes);
+router.use("/level", levelRoutes);
 
 module.exports = router;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,47 +1,10 @@
 const router = require("express").Router();
 const gameController = require("../../controllers/gameController");
 
-// matches with '/api/user'
+// matches with '/api/user?display_name=reptile18'
 router.route("/")
-  .get(gameController.findAll)
-  .post(gameController.createUser);
-
-// matches with '/api/user/:email'
-router
-  .route("/email/:email")
-  .get(gameController.findUserByEmail);
-
-// matches with '/api/user/:email'
-router
-  .route("/display_name/:display_name")
-  .get(gameController.findUserByDisplayName)
-  .put(gameController.updateUserByDisplayName);
-
-
-// router.post("/api/user", (req,res) => {
-//   console.log("in user post route");
-//   const user = req.body;
-
-//   gameController.User.create(user).then(dbUser => {
-//     res.json(dbUser);
-//   }).catch(err => {
-//     res.json(err);
-//   });
-// });
-
-// router.get("/api/user/:email", (req,res) => {
-//   gameController.User.find({ "email": req.params.email}).then( dbUser => {
-//     res.json(dbUser);
-//   }).catch(err => {
-//     res.json(err);
-//   });
-// })
-
-// probs delete this
-// router.route("/")
- 
-// router
-//   .route("/:id")
-  
+  .get(gameController.findUser)
+  .post(gameController.createUser)
+  .put(gameController.updateUserByDisplayName); 
 
 module.exports = router;
