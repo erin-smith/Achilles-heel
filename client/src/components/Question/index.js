9e8849fa3d96d2d8a044dc531970102dd5e2c024
Johnny Li <reptile18@gmail.com>
2020-08-27 15:59:01 -0700

    fix warnings (#47)
    Co-authored-by: alexastef <62451633+alexastef@users.noreply.github.com>

--- client/src/components/Question/index.js
+++ client/src/components/Question/index.js
@@ -28,9 +28,6 @@ const styles = {
 function Question(props) {
   const [showQuestion, setShowQuestion] = useState(false);
 
-  // function onCloseQuestion() {
-  //   // set provider?
-  // }
 
   function handleAnswerClick(correct) {
     if (correct) {
