12b5706f85da5d9ec3dee43d08dad0a4f5c3a74c
Johnny Li <reptile18@gmail.com>
2020-08-27 17:01:26 -0700

    lint fixes for server routes and controller (#48)
    * lint fixes
    
    * change camelcase to warning

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,54 +1,58 @@
 const db = require("../models");
 
 module.exports = {
-  createUser: function (req, res) {
-    db.User.create(req.body).then(dbUser => res.json(dbUser)).catch(err => res.status(422).json(err));
+  createUser(req, res) {
+    db.User.create(req.body)
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(422).json(err));
   },
-  findUser: function (req, res) {
-    const email = req.query.email;
-    const display_name = req.query.display_name;
+  findUser(req, res) {
+    const { email } = req.query;
+    const { display_name } = req.query;
     if (display_name) {
-      db.User.find({ display_name: display_name}).then(dbUser => res.json(dbUser)).catch(err => res.status(422).json(err));
+      db.User.find({ display_name })
+        .then((dbUser) => res.json(dbUser))
+        .catch((err) => res.status(422).json(err));
+    } else {
+      db.User.find({ email })
+        .then((dbUser) => res.json(dbUser))
+        .catch((err) => res.status(422).json(err));
     }
-    else {
-      db.User.find({ email: email}).then(dbUser => res.json(dbUser)).catch(err => res.status(422).json(err));
-    }
   },
-  updateUserByDisplayName: function (req,res) {
-    console.log("updating user", req.query.display_name)
-    db.User.findOneAndUpdate({display_name: req.query.display_name}, req.body).then(dbUser => res.json(dbUser)).catch(err => res.status(422).json(err));
+  updateUserByDisplayName(req, res) {
+    console.log("updating user", req.query.display_name);
+    db.User.findOneAndUpdate({ display_name: req.query.display_name }, req.body)
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(422).json(err));
   },
-  createOverworld: function (req, res) {
-    db.World.create(req.body).then(dbWorld => req.json(dbWorld)).catch(err => res.status(422).json(err));
+  createOverworld(req, res) {
+    db.World.create(req.body)
+      .then((dbWorld) => req.json(dbWorld))
+      .catch((err) => res.status(422).json(err));
   },
-  findOverworld: function (req, res) {
-    db.World.find({name: req.query.name}).then(dbWorld => res.json(dbWorld)).catch(err => res.status(422).json(err));
+  findOverworld(req, res) {
+    db.World.find({ name: req.query.name })
+      .then((dbWorld) => res.json(dbWorld))
+      .catch((err) => res.status(422).json(err));
   },
-  createArena: function (req, res) {
-    db.Level.create(req.body).then(dbLevel => res.json(dbLevel)).catch(err => res.status(422).json(err));
+  createArena(req, res) {
+    db.Level.create(req.body)
+      .then((dbLevel) => res.json(dbLevel))
+      .catch((err) => res.status(422).json(err));
   },
-  findArenaById: function (req, res) {
-    db.Level.findById(req.query.id).then(dbLevel => res.json(dbLevel)).catch(err => res.status(422).json(err));
+  findArenaById(req, res) {
+    db.Level.findById(req.query.id)
+      .then((dbLevel) => res.json(dbLevel))
+      .catch((err) => res.status(422).json(err));
   },
-  createQuestion: function (req, res) {
-    db.Questions.create(req.body).then(dbQuestion => res.json(dbQuestion)).catch(err => res.status(422).json(err));
+  createQuestion(req, res) {
+    db.Questions.create(req.body)
+      .then((dbQuestion) => res.json(dbQuestion))
+      .catch((err) => res.status(422).json(err));
   },
-  findQuestionsByTopic: function (req, res) {
-    db.Questions.find({topic: req.query.topic}).then(dbQuestions => res.json(dbQuestions)).catch(err => res.status(422).json(err));
-  },
-  findAll: function(req, res) {
-   
-  },
-  findById: function(req, res) {
-    
-  },
-  create: function(req, res) {
-    
-  },
-  update: function(req, res) {
-   
-  },
-  remove: function(req, res) {
-    
+  findQuestionsByTopic(req, res) {
+    db.Questions.find({ topic: req.query.topic })
+      .then((dbQuestions) => res.json(dbQuestions))
+      .catch((err) => res.status(422).json(err));
   }
 };
