2d2ecea990a3e26e2812342a64a67f29e2265444
Johnny Li <reptile18@gmail.com>
2020-08-28 10:52:46 -0700

    more fixes for test component

--- client/src/components/Test/index.js
+++ client/src/components/Test/index.js
@@ -1,116 +1,115 @@
-import React, { useState } from 'react';
-import API from '../../utils/API';
-import { Grid, Typography } from '@material-ui/core'
+import React, { useState } from "react";
+import { Grid, Typography } from "@material-ui/core";
+import API from "../../utils/API";
 
 const display_name = "minotaur";
 
 const user = {
   email: "minotaur@gmail.com",
-  display_name: display_name,
+  display_name,
   score: 1
-}
+};
 
 const updatedUser = {
   email: "minotaur@gmail.com",
-  display_name: display_name,
+  display_name,
   score: 17
-}
+};
 
 const overworld = {
   name: "Ancient China",
   background_image: "zzzplaceholderzzz"
-}
+};
 
 const level = {
-  "name": "Minotaur's Labyrinth",
-  "score_points": 3000,
-  "difficulty": "5",
-  "topic": "Myth_and_Legend"
-}
+  name: "Minotaur's Labyrinth",
+  score_points: 3000,
+  difficulty: "5",
+  topic: "Myth_and_Legend"
+};
 
 const question = {
-  "question": "How are you holding up?",
-  "image": "zzzplaceholderimg",
-  "answerOptions": [
-      {
-          "answerBody": "Good"
-      },
-      {
-          "answerBody": "Bad"
-      },
-      {
-          "answerBody": "Meh",
-          "isCorrect": true
-      },
-      {
-          "answerBody": "Fine"
-      }
+  question: "How are you holding up?",
+  image: "zzzplaceholderimg",
+  answerOptions: [
+    {
+      answerBody: "Good"
+    },
+    {
+      answerBody: "Bad"
+    },
+    {
+      answerBody: "Meh",
+      isCorrect: true
+    },
+    {
+      answerBody: "Fine"
+    }
   ],
-  "category": "A",
-  "points": 100,
-  "topic": "Greetings"
-}
-
+  category: "A",
+  points: 100,
+  topic: "Greetings"
+};
 
 function Test() {
-  const [output,setOutput] = useState("");
-  
+  const [output, setOutput] = useState("");
+
+  function out(obj) {
+    setOutput(JSON.stringify(obj));
+  }
+
   function onCreateUserClicked() {
     API.createUser(user)
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
   
   function onUpdateUserClicked() {
     API.saveUser(display_name, updatedUser)
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   function onGetUserClicked() {
     API.getUser("reptile18")
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   function onOverworldCreateClicked() {
     API.createOverworld(overworld)
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   function onOverworldGetClicked() {
     API.findOverworld("Greece")
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   function onLevelCreateClicked() {
     API.createArena(level)
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   function onLevelGetClicked() {
     API.findArena("5f46aaa0040aa52bb0a56ce4")
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   function onQuestionCreateClicked() {
     API.createQuestion(question)
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   function onQuestionGetClicked() {
     API.findQuestions("Myth_and_Legend")
-    .then((response) => out(response.data))
-    .catch((err) => console.log(err));
-  }
-
-  function out(obj) {
-    setOutput(JSON.stringify(obj));
+      .then((response) => out(response.data))
+      .catch((err) => console.log(err));
   }
 
   return (
