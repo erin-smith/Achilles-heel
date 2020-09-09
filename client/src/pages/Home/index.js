89bbb7ccae8aa9867e435caed46b597f46bd7983
Erin Smith <erin.acumen@gmail.com>
2020-09-08 18:49:58 -0700

    Appbar (#110)
    * 12 monster questons for Hades trivia
    
    * add image, fix responsiveness of appbar

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import { Grid, Paper } from "@material-ui/core";
 import GoogleButton from "../../components/Login/GoogleButton";
 
+const image = {
+  url: "https://i.pinimg.com/564x/69/91/99/699199779efef18eb7025491e989e006.jpg",
+  title: "Ancient Greek Gods",
+  width: "100%",
+  marginTop: "30px",
+};
+
 function Home() {
   return (
     <Grid container direction="column" justify="center" alignItems="center">
-      <h1>Achilles&apos; Heel</h1>
       <Paper>
-        Placeholder for Image
+        <GoogleButton styles={{ marginBottom: "30px" }} />
+        <div />
+        <img width={image.width} src={image.url} alt="Achilles" />
       </Paper>
-      <GoogleButton />
     </Grid>
   );
 }
