eaf6ea0bfa8e9520347c27b8685118e47a500f0d
Erin Smith <erin.acumen@gmail.com>
2020-09-08 17:14:13 -0700

    add image, fix responsiveness of appbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,45 +1,43 @@
 import React, { useEffect } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Menu, MenuItem, IconButton, Avatar } from "@material-ui/core";
-import { useStore } from "../../utils/globalState";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+  IconButton,
+  Avatar,
+} from "@material-ui/core";
 import { withRouter } from "react-router-dom";
+import { useStore } from "../../utils/globalState";
 
 const logo = {
-  src:require('../../assets/achilles_heel_logo3.png'),
-  width:"40px"
-}
+  src: require("../../assets/achilles_heel_logo2.png"),
+  width: "40px"
+};
 
 const appImg = {
-  src:require('../../assets/appbar_img.png'),
-  width:"40px"
-}
+  src: require("../../assets/appbar_img.png"),
+  width: "100%",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center center",
+  backgroundSize: "cover",
+  backgroundAttachment: "fixed",
+  height: "100%",
+};
 
+const font = "'Caesar Dressing', cursive";
 
-const font =  "'Caesar Dressing', cursive";
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
   },
-buttonBase: {
-    marginRight: theme.spacing(1),
-  },
   title: {
     flexGrow: 1,
-    marginRight: theme.spacing(110),
-    marginLeft: theme.spacing(0),
-    fontFamily: font, 
-    fontSize: "50px", 
+    fontFamily: font,
+    fontSize: "40px",
     color: "charcoal",
-  },
-  AppBar: {
-backgroundImage: "url('../../assets/appbar_img.png')",
-backgroundRepeat: "no-repeat",
-backgroundPosition: "center center",
-backgroundSize: "cover",
-backgroundAttachment: "fixed",
-height: "100%",
-MuiAppBarcolor: appImg
   }
 }));
 
@@ -73,45 +71,49 @@ const Header = (props) => {
 
   return (
     <div className={classes.root}>
-    <AppBar position="static" src={appImg.src} alt="stained stone"  >
-      <Toolbar >
-        <img width={logo.width} src={logo.src} alt="Achilles" ></img>
-            <Typography variant="h6" className={classes.title}>
+      <AppBar position="static" src={appImg.src} alt="stained stone">
+        <Toolbar>
+          <img edge="start" style={{ marginRight: "10px" }} width={logo.width} src={logo.src} alt="Achilles" />
+          <Typography variant="h6" className={classes.title}>
             Achilles Heel
           </Typography>
-               <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <Avatar src="https://avatars.dicebear.com/api/bottts/example.svg?options[colors][]=cyan" ></Avatar>
-                </IconButton> 
-          <Typography variant="h6" > Total Score: {`${state.user.score}`}</Typography>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={ () => setAnchorEl(null)} >
-                  <MenuItem onClick={() => handleMenuClick("/")}>Home</MenuItem>
-                  <MenuItem onClick={() => handleMenuClick("/login")}>My Profile</MenuItem>
-                <MenuItem onClick={() => handleMenuClick("/about")}>About</MenuItem>
-                <MenuItem onClick={() => handleMenuClick("/logout")}>Logout</MenuItem>
-              </Menu>
-      </Toolbar>
-    </AppBar>
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <Avatar>{`${state.user.avatar}`}</Avatar>
+          </IconButton>
+          <Typography variant="h6">
+            Total Score:
+            {`${state.user.score}`}
+          </Typography>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={open}
+            onClose={() => setAnchorEl(null)}
+          >
+            <MenuItem onClick={() => handleMenuClick("/")}>Home</MenuItem>
+            <MenuItem onClick={() => handleMenuClick("/login")}>My Profile</MenuItem>
+            <MenuItem onClick={() => handleMenuClick("/about")}>About</MenuItem>
+            <MenuItem onClick={() => handleMenuClick("/logout")}>Logout</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
     </div>
   );
-}
+};
 
 export default withRouter(Header);
