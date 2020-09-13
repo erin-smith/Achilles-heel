243a68a8239975c2f2e228e262440f2da2018e1b
Johnny Li <reptile18@gmail.com>
2020-09-13 14:17:44 -0700

    remove CardActionArea import

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Container,
-  Card, CardActionArea,
+  Card,
   Button,
   TextField,
   Typography,
